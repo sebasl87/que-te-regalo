38f09c56c9d3371e33b54948b741767e735c59dc
Sebastian R Loguzzo <Sebastian.loguzzo@gmail.com>
2023-02-14 15:39:55 -0300

    Added login with G+expo

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo, useEffect, useCallback } from "react";
 import Constants from "expo-constants";
 import * as Google from "expo-auth-session/providers/google";
-// import firebase from "firebase";
+
 import Firebase from "./Firebase";
 
 import { Text, View, StatusBar } from "react-native";
@@ -52,15 +52,13 @@ export default function App() {
   const auth = getAuth(Firebase);
 
   useEffect(() => {
-    const user = auth.currentUser;
-    user && (setUserLogged(user ? true : false), setUserProfile(user));
-
-    // const authListener = onAuthStateChanged((auth, (user)) => {
-    //   setUserLogged(user ? true : false);
-    //   setIsLoading(false);
-    //   setUserProfile(user);
-    // });
-    // return authListener;
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserProfile(user);
+      } else {
+        console.log("Fail login");
+      }
+    });
   }, []);
 
   const doLogin = async (email, password) => {
@@ -79,45 +77,19 @@ export default function App() {
     );
   };
 
-  useEffect(() => {
-    if (response?.type === "success") {
-      const { authentication } = response;
-    }
-  }, [response]);
   const [request, response, promptAsync] = Google.useAuthRequest({
     expoClientId: Constants.manifest.extra.EXPO_KEY, //From app.json
     iosClientId: Constants.manifest.extra.IOS_KEY, //From app.json
     androidClientId: Constants.manifest.extra.ANDROID_KEY, //From app.json
   });
-  
-  // const Glogin = () => {
-    // try {
-    //   //await GoogleSignIn.askForPlayServicesAsync();
-    //   const result = await Google.logInAsync({
-    //     //return an object with result token and user
-    //     iosClientId: Constants.manifest.extra.IOS_KEY, //From app.json
-    //     androidClientId: Constants.manifest.extra.ANDROID_KEY, //From app.json
-    //   });
-    //   if (result.type === "success") {
-    //     console.log(result);
-    //     setIsLoading(true);
-    //     const credential = GoogleAuthProvider.credential(
-    //       //Set the tokens to Firebase
-    //       result.idToken,
-    //       result.accessToken
-    //     );
-    //     signInWithCredential(credential) //Login to Firebase
-    //       .catch((error) => {
-    //         console.log(error);
-    //       });
-    //   } else {
-    //     //CANCEL
-    //     console.log("CANCEL")
-    //   }
-    // } catch ({ message }) {
-    //   alert("login: Error:" + message);
-    // }
-  // };
+
+  const Glogin = () => {
+    try {
+      promptAsync();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const mainC = useMemo(
     () => ({
@@ -131,7 +103,7 @@ export default function App() {
       },
       handleGLogin: () => {
         //The new login with google handler available to context
-        promptAsync();
+        Glogin();
       },
     }),
     []
