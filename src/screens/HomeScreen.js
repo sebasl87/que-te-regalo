a40a50c153be3a2e2c12bc6ab36fc45e875e31bf
Exequiel Sosa <exequielsosa@gmail.com>
2023-02-14 02:46:13 -0300

    feat: start table

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,14 +2,21 @@ import { Text, View } from "react-native";
 import MenuTop from "../components/MenuTop.js";
 import NoItems from "../components/NoItems.js";
 import AddButton from "../components/AddButton.js";
+import Table from "../components/Table.js";
 
 function HomeScreen() {
-    const isEmpty = true;
-    const Tabla = () => {
-        return (
-            <Text>Soy una Tabla</Text>
-        );
-    }
+    const isEmpty = false;
+
+    const whishes = [
+        { text: "Pelota de futbol mundial qatar", disabled: false, id: 0 },
+        { text: "Playstation 5", disabled: false, id: 1 },
+        { text: "Xbox series X", disabled: false, id: 2 },
+        { text: "Remera oficial Argentina 3 estrellas", disabled: false, id: 3 },
+        { text: "Remera Nike San Lorenzo Alternativa", disabled: false, id: 4 },
+        { text: "God Of War Ragnarok", disabled: true, id: 5 },
+        { text: "Abono GamePass", disabled: false, id: 6 },
+        { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 7 },
+    ];
 
     return (
         <>
@@ -18,7 +25,9 @@ function HomeScreen() {
             {isEmpty ?
                 <NoItems />
                 :
-                <><Tabla /></>
+                
+                <Table wishName="algo" />
+                
             }
 
         </>
