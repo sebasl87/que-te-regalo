17c2eb8be6a58845df01460b4e741e0bf1f2d8c9
Exequiel Sosa <exequielsosa@gmail.com>
2023-02-16 23:02:02 -0300

    delete

--- src/constants.js
+++ src/constants.js
@@ -18,31 +18,6 @@ export const BOTONES_SOCIALES_REGISTER = [
   { text: "Registrarme con Google", name: "google", id: 2 },
 ];
 
-export const WHISHES = [
-  { text: "Pelota de futbol mundial qatar", disabled: false, id: 0 },
-  { text: "Playstation 5", disabled: false, id: 1 },
-  { text: "Xbox series X", disabled: false, id: 2 },
-  { text: "Remera oficial Argentina 3 estrellas", disabled: false, id: 3 },
-  { text: "Remera Nike San Lorenzo Alternativa", disabled: false, id: 4 },
-  { text: "God Of War Ragnarok", disabled: true, id: 5 },
-  { text: "Abono GamePass", disabled: false, id: 6 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 7 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 8 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 9 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 10 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 11 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 12 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 13 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: true, id: 14 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 15 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 16 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 17 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: true, id: 18 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 19 },
-  { text: "Zapatillas Running Nikle talle 45", disabled: false, id: 20 },
-  { text: "Zapatillas Running Nikle talle 60", disabled: true, id: 21 },
-];
-
 export const MORE_WISHES = {
   "data": {
     "getWishlistUsers": {
